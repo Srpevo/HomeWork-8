ef1fa9fdc3f721efc4b57b043774df7fdb446bb1
Srpevo <srprevo000@gmail.com>
2024-11-19 17:06:21 +0400

    Update EmailValidator.cs

--- HomeWork-8r/Utils/Validators/DataValidators/EmailValidator/EmailValidator.cs
+++ HomeWork-8r/Utils/Validators/DataValidators/EmailValidator/EmailValidator.cs
@@ -20,16 +20,16 @@ namespace HomeWork_8.Utils.Validators.DataValidators.EmailValidator
                 throw new IncorrectEmailEntryException("email cannot be empty");
 
             var Regx = new Regex(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"); //using regular expression for more realism ;)
-            string[] WordsToRemove = { "mail", "com", "gmail", "@", "." };
-            string CorrectEmail = email;
-            
-            foreach (string word in WordsToRemove)
-            {
-                CorrectEmail = CorrectEmail.Replace(word, "");
-            }
             
             if (!Regx.IsMatch(email))
             {
+                string[] WordsToRemove = { "mail", "com", "gmail", "@", "." };
+                string CorrectEmail = email;
+                
+                foreach (string word in WordsToRemove)
+                {
+                    CorrectEmail = CorrectEmail.Replace(word, "");
+                }
                 throw new IncorrectEmailEntryException($"incorrect email, correct > {CorrectEmail}@gmail.com");
             }
               
