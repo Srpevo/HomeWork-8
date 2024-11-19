fb1650369d7d03dd7bae7742de5e723e685d0012
Srpevo <srprevo000@gmail.com>
2024-11-19 19:02:53 +0400

    Add files via upload

---
+++ HomeWork-8r/Utils/Validators/DataValidators/CashValidator/CashValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_8r.Utils.Validators.DataValidators.CashValidator
+{
+    internal static class CashValidator
+    {
+        public static bool CheckCash(decimal cash)
+        {
+            if (cash < 0) throw new ArgumentException("money can't be negative");
+            return true;
+        }
+    }
+}
---
+++ HomeWork-8r/Utils/Validators/DataValidators/IdValidator/IdValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_8r.Utils.Validators.DataValidators.IdValidator
+{
+    internal static class IdValidator
+    {
+        public static bool CheckUserId(string id)
+        {
+            if (string.IsNullOrEmpty(id)) throw new ArgumentNullException("id cannot be empty");
+            return true;
+        }
+    }
+}
